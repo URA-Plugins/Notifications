3fc8bcb6aee4d0b520522eed9af720ddbc1e2bec
离披 <lipi@shuise.dev>
2025-06-28 20:33:11 +0800

    添加项目文件。

---
+++ Class1.cs
@@ -0,0 +1,66 @@
+﻿using Microsoft.Toolkit.Uwp.Notifications;
+using Newtonsoft.Json.Linq;
+using Spectre.Console;
+using System.IO.Compression;
+using UmamusumeResponseAnalyzer;
+using UmamusumeResponseAnalyzer.Plugin;
+using YamlDotNet.Serialization;
+
+namespace Notifications
+{
+    public class Notifications : IPlugin
+    {
+        public string Name => "Notifications";
+        public Version Version => new(1, 0, 0);
+        public string Author => "";
+        public async Task UpdatePlugin(ProgressContext ctx)
+        {
+            var progress = ctx.AddTask($"[UAFScenarioAnalyzer] Update");
+
+            using var client = new HttpClient();
+            using var resp = await client.GetAsync($"https://api.github.com/repos/URA-Plugins/{Name}/releases/latest");
+            var json = await resp.Content.ReadAsStringAsync();
+            var jo = JObject.Parse(json);
+
+            var isLatest = ("v" + Version.ToString()).Equals("v" + jo["tag_name"]?.ToString());
+            if (isLatest)
+            {
+                progress.Increment(progress.MaxValue);
+                progress.StopTask();
+                return;
+            }
+            progress.Increment(25);
+
+            using var msg = await client.GetAsync(jo["assets"][0]["browser_download_url"].ToString(), HttpCompletionOption.ResponseHeadersRead);
+            using var stream = await msg.Content.ReadAsStreamAsync();
+            var buffer = new byte[8192];
+            while (true)
+            {
+                var read = await stream.ReadAsync(buffer);
+                if (read == 0)
+                    break;
+                progress.Increment(read / msg.Content.Headers.ContentLength ?? 1 * 0.5);
+            }
+            using var archive = new ZipArchive(stream);
+            archive.ExtractToDirectory(Path.Combine("Plugins", Name), true);
+            progress.Increment(25);
+
+            progress.StopTask();
+        }
+        [Analyzer]
+        public void OnTrainingFinish(JObject jo)
+        {
+            if (!jo.HasCharaInfo()) return;
+            if (jo["data"] is null || jo["data"] is not JObject data) return;
+            if (data["chara_info"] is null || data["chara_info"] is not JObject chara_info) return;
+            var state = chara_info["state"].ToInt();
+            if (state is 2 or 3 && data["unchecked_event_array"]?.Count() == 0)
+            {
+                // Requires Microsoft.Toolkit.Uwp.Notifications NuGet package version 7.0 or greater
+                new ToastContentBuilder()
+                    .AddText("育成结束力！")
+                    .Show(); // Not seeing the Show() method? Make sure you have version 7.0, and if you're using .NET 6 (or later), then your TFM must be net6.0-windows10.0.17763.0 or greater
+            }
+        }
+    }
+}

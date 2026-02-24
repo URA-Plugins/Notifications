e4d34e054180eb7f9bd1fb21d11add3ab5d629bb
离披 <lipi@shuise.dev>
2026-02-24 18:06:27 +0800

    适配breeders

--- Class1.cs
+++ Class1.cs
@@ -12,19 +12,18 @@ namespace Notifications
     {
         [PluginDescription("育成结束时发出通知")]
         public string Name => "Notifications";
-        public Version Version => new(1, 0, 0);
         public string Author => "离披";
         public string[] Targets => [];
         public async Task UpdatePlugin(ProgressContext ctx)
         {
-            var progress = ctx.AddTask($"[{Name}] 更新");
+            var progress = ctx.AddTask($"[[{Name}]] 更新");
 
             using var client = new HttpClient();
             using var resp = await client.GetAsync($"https://api.github.com/repos/URA-Plugins/{Name}/releases/latest");
             var json = await resp.Content.ReadAsStringAsync();
             var jo = JObject.Parse(json);
 
-            var isLatest = ("v" + Version.ToString()).Equals("v" + jo["tag_name"]?.ToString());
+            var isLatest = ("v" + ((IPlugin)this).Version.ToString()).Equals("v" + jo["tag_name"]?.ToString());
             if (isLatest)
             {
                 progress.Increment(progress.MaxValue);
